08f838ac5bb0a4369089d2c6b773d4e793875aac
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2026-02-18 16:46:55 +0100

    add not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { LoggedInPage } from './core/pages/logged-in-page/logged-in-page';
 import { appRoutesEnum } from './core/enums/routes.enum';
+import { NotFound } from './core/pages/not-found/not-found';
 
 export const routes: Routes = [
   {
     path: appRoutesEnum.turnedOn,
     component: LoggedInPage,
+  },
+  {
+    path: '**',
+    component: NotFound,
   },
   // {
   //   path: appRoutesEnum.tunredOff,
